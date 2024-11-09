205af5d03e2005687ed92de2e23765956b2ccb51
BanaantjeJowie <boonej394@gmail.com>
2024-11-09 11:47:46 +0100

    m

--- resources/Js/script.js
+++ resources/Js/script.js
@@ -1,17 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    emailjs.init("8i02dt3qU_5F7fR0U"); 
-    document.getElementById("contactForm").addEventListener("submit", function(event) {
-        event.preventDefault();
-
-        emailjs.send("service_cgza982", "template_xwbpkv1", {
-            naam: document.getElementById("naam").value,
-            email: document.getElementById("email").value,
-            telefoon: document.getElementById("telefoon").value,
-            bericht: document.getElementById("bericht").value
-        }).then(function(response) {
-            alert("Bericht verzonden! Dank u voor uw bericht.");
-        }, function(error) {
-            alert("Er is een fout opgetreden bij het verzenden van het bericht.");
-        });
-    });
+window.addEventListener("scroll", function() {
+    const scrollButton = document.getElementById("scroll-to-top");
+    if (window.scrollY > 100) { // Show button after scrolling down 100px
+        scrollButton.classList.add("show");
+    } else {
+        scrollButton.classList.remove("show");
+    }
 });
